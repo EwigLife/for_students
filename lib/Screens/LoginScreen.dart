7f82b037b1f90ed05fcb815850908b2a5108b4b8
syed Zain ul hassan qadri <syedqadri@syeds-MacBook-Pro.local>
2020-09-14 18:30:34 +0500

    commit

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -6,10 +6,119 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: kPrimaryColor,
-      child: Text('Home'),
-    );
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: Text('WelCome',
+                    style: TextStyle(
+                      fontSize: 50.0,
+                    fontWeight: FontWeight.bold,
+                    ),
+                     ),
+                  ),
+                  SizedBox(width: 10,),
+                  Container(
+                    child: Text('!',
+                    style: TextStyle(
+                      fontSize: 50.0,
+                    fontWeight: FontWeight.bold,
+                    color: kPrimaryColor,
+                    ),
+                     ),
+                  ),
+                ],
+              ),
+              Container(
+                child: Text('EwigLife',
+                style: TextStyle(
+                      fontSize: 50.0,
+                    fontWeight: FontWeight.bold,
+                    color: kPrimaryColor,),
+                     ),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Email'),
+                ),
+                SizedBox(
+                  height: 40.0,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Password'),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Container(
+                    alignment: Alignment.bottomRight,
+                    padding: EdgeInsets.only(top: 15.0,),
+                    child: InkWell(
+                      child: Text('Forgot Password ?',
+                      style: TextStyle(color: kPrimaryColor,),
+                      ),
+                       ),
+                  ),
+                ),
+                SizedBox(height: 40.0,),
+                Padding(padding: EdgeInsets.all(15.0),
+                child: Container(
+                  height: 50.0,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: kPrimaryColor,
+                    shadowColor: Colors.black12,
+                    elevation: 7.0,
+                    child: GestureDetector(
+                      onTap: (){},
+                      child: Center(
+                        child: Text('Login',
+                        style: TextStyle(color: Colors.white,
+                        fontSize: 25.0,),),
+                      ),
+                    ),
+
+                  ),
+
+                ),
+                ),
+                SizedBox(height: 40.0,),
+                Padding(padding: EdgeInsets.all(15.0),
+                child: Container(
+                  height: 50.0,
+                  child: Material(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: kPrimaryColor,
+                    shadowColor: Colors.black12,
+                    elevation: 7.0,
+                    child: GestureDetector(
+                      onTap: (){},
+                      child: Center(
+                        child: Text('Social Media',
+                        style: TextStyle(color: Colors.white,
+                        fontSize: 25.0,),),
+                      ),
+                    ),
+
+                  ),
+
+                ),
+                ),
+                ],
+            ),
+          ),
+
+
+      ),
     
+    );
   }
 }
--- lib/Screens/SplashScreen.dart
+++ lib/Screens/SplashScreen.dart
@@ -11,60 +11,60 @@ class SplashScreen extends StatefulWidget {
 }
 class _SplashScreenState extends State<SplashScreen> {
   
-  // startTime() async {
-  //   var _duration = Duration(seconds: 5);
-  //   return Timer(_duration, navigationPage);
-  // }
+  startTime() async {
+    var _duration = Duration(seconds: 5);
+    return Timer(_duration, navigationPage);
+  }
 
-  // void navigationPage() {
-  //   Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => LoginPage()), (_) => false);
-  // }
+  void navigationPage() {
+    Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => LoginPage()), (_) => false);
+  }
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   startTime();
-  // }
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(color: kPrimaryColor,),
-          ),
-          Column(
-            children: <Widget>[
-              Expanded(
-                flex: 2,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    CircleAvatar(
-                      backgroundColor: kCircularAvatarBGColor, 
-                      radius: 100.0,
-                      child: ClipOval(child: Image.network('https://www.kindpng.com/picc/m/355-3557482_flutter-logo-png-transparent-png.png')),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top:10.0,),
-                      child: Text('Awsome App Development', 
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
+          fit: StackFit.expand,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(color: kPrimaryColor,),
+            ),
+            Column(
+              children: <Widget>[
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: kCircularAvatarBGColor, 
+                        radius: 100.0,
+                        child: ClipOval(child: Image.network('https://www.kindpng.com/picc/m/355-3557482_flutter-logo-png-transparent-png.png')),
                       ),
+                      Padding(
+                        padding: EdgeInsets.only(top:10.0,),
+                        child: Text('Awsome App Development', 
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        ),
                       ),
-                    ),
-                  ],
-                ), 
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+                    ],
+                  ), 
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
   }
 }
